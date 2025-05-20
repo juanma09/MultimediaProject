ddad0077cd8192147dc8cf41b3e99ba792b10026
juanma09 <mateoelizeche09@gmail.com>
2025-05-21 01:47:55 +0800

    Renaming

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -9,6 +9,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2 moveInput;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
